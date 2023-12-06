365a4bc35bc95a1cf98b8ed24b98039c6d3fbcc4
PovilasU <povilas.urbonas85@gmail.com>
2023-12-06 21:41:21 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import {  } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Hello World</h1>
+       
+      </div>
+    
+    
+    </>
+  )
+}
+
+export default App
