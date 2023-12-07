adaeddb9eb7d33e486baadcc6651e96fd42aa351
PovilasU <povilas.urbonas85@gmail.com>
2023-12-07 19:39:38 +0000

    Updated about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,41 @@
-
 import React from 'react';
 
 const About: React.FC = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the About component.</p>
+    <div className="module">
+      <img className="img-me center" src="/povilas.jpg" alt="image" />
+      <h2 className="center about-h2">Description</h2>
+      <p className="tac1">
+        A web app that fetches geolocation data from one API and uses that to
+        fetch weather data from another.
+      </p>
+      <h2>Technologies used</h2>
+      <ul>
+        <li>ReactJs</li>
+        <li>TypeScript</li>
+        <li>React Router</li>
+        <li>Fetch API</li>
+        <li>https://api.open-meteo.com</li>
+        <li>https://geocoding-api.open-meteo.com/</li>
+      </ul>
+      <div className="contact-links jcse">
+        <a
+          href="https://www.linkedin.com/in/povilas-urbonas-0a6a53a4/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/PovilasU/povilas-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
     </div>
   );
-};
+}
 
 export default About;
